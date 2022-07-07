a2c08b2a8c59534b2ca9a5fd6c0cbc70fbeaca66
zzy1157654912 <1157654912@qq.com>
2022-07-07 17:43:40 +0800

    update:root-search

--- react-hooks-frontend/src/components/Root/Root.js
+++ react-hooks-frontend/src/components/Root/Root.js
@@ -1,28 +1,134 @@
 import './Root.css';
 import LeftSide from "../leftSide/LeftSide";
-import React , { useState }from "react";
-import { Space, Table, Tag, Checkbox, Divider, Row, Col } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import React , { useState,useRef }from "react";
+import { Space, Table, Tag, Checkbox, Divider, Row, Col,Button, Input } from 'antd';
+import Highlighter from 'react-highlight-words';
+
 const CheckboxGroup = Checkbox.Group;
 const plainOptions = ['部门权限', '人员权限'];
 const defaultCheckedList = ['人员权限'];
 
+
 const Root = () => {
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1890ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
     const columns = [
         {
             title: '姓名',
             dataIndex: 'name',
             key: 'name',
-            render: (text) => <a>{text}</a>,
+            ...getColumnSearchProps('name'),
+            // render: (text) => <a>{text}</a>,
         },
         {
             title: '职位',
             dataIndex: 'position',
             key: 'position',
+            ...getColumnSearchProps('position'),
         },
         {
             title: '所处部门',
             dataIndex: 'department',
             key: 'department',
+            ...getColumnSearchProps('department'),
         },
         {
             title: '权限',
@@ -39,10 +145,10 @@ const Root = () => {
                     >
                         <Row>
                             <Col span={8}>
-                                <Checkbox value="部门权限">部门权限</Checkbox>
+                                <Checkbox value="部门权限" disabled={true}>部门权限</Checkbox>
                             </Col>
                             <Col span={8}>
-                                <Checkbox value="人员权限">人员权限</Checkbox>
+                                <Checkbox value="人员权限" disabled={true}>人员权限</Checkbox>
                             </Col>
                         </Row>
                     </Checkbox.Group>
